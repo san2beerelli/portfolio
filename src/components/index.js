27a3191baf6241e1449edcf5127c34f19ad20b80
Santhosh Beerelli <san2beerelli@gmail.com>
2018-02-18 16:32:42 -0500

    Removed Paper from components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Header } from "./Header/Header";
 export { default as BurgerButton } from "./BurgerButton/BurgerButton";
 export { default as Background } from "./Background/Background";
-export { default as Paper } from "./Paper/Paper";
